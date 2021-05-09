fe48a365a851e747758218158bbb674e2ff69b78
Tevin <tevinward7@gmail.com>
2021-05-09 05:26:53 -0400

    made movie input box fetch data

--- src/App.js
+++ src/App.js
@@ -24,9 +24,6 @@ function App() {
 
       </section>
 
-
-
-
     </div>
   );
 }
--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -1,11 +1,31 @@
 import React from "react";
+import API from "../utils/API"
 
 const MovieSearch = () => {
 
+    const [searchQuery, setSearchQuery] = React.useState();
+    const [movieTitle, setMovieTitle] = React.useState();
+    const [movieYear, setMovieYear] = React.useState();
+
+    const handleSearch = (search) => {
+
+        setSearchQuery(search);
+
+        API.searchTitle(search)
+            .then(response => {
+                setMovieTitle(response.data.Title);
+                setMovieYear(response.data.Year)
+            })
+
+    }
+
     return (
         <>
+
             <h6>Movie Title</h6>
-            <input></input>
+
+            <input value={searchQuery} onChange={e => handleSearch(e.target.value)}></input>
+
         </>
     )
 
