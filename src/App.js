707c785702317940bb11099de9d1e4a78dc44bd0
Tevin <tevinward7@gmail.com>
2021-05-08 23:57:06 -0400

    put in some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import MovieSearch from './components/MovieSearch/MovieSearch';
+import SearchResults from './components/SearchResults/SearchResults';
+import Nominations from './components/Nominations/Nominations';
+
 
 function App() {
   return (
     <div className="App">
-
+      <MovieSearch />
+      <SearchResults />
+      <Nominations />
     </div>
   );
 }
---
+++ src/components/MovieSearch/MovieSearch.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieSearch = () => {
+
+    return (
+        <>
+            <h6>Movie Title</h6>
+            <input></input>
+        </>
+    )
+
+}
+
+export default MovieSearch;
